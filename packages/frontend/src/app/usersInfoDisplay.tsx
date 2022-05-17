ff0ce30590ab69109fc2ca3262c121c170fdac26
maxfo <max.fom.vic@gmail.com>
2022-05-17 22:58:21 +0500

    First version userInfoDisplay(corrected)

--- packages/frontend/src/app/usersInfoDisplay.tsx
+++ packages/frontend/src/app/usersInfoDisplay.tsx
@@ -1,64 +1,70 @@
 import React from 'react';
-import fullLife from './img/FilledHearth.svg'
-import emptyLife from './img/EmptyHearth.svg'
-import fullMana from './img/FilledMana.svg'
-import emptyMana from './img/EmptyMana.svg'
+import fullLife from './img/FilledHearth.svg';
+import emptyLife from './img/EmptyHearth.svg';
+import fullMana from './img/FilledMana.svg';
+import emptyMana from './img/EmptyMana.svg';
 import './App.css';
 
 const maxHealth = 3;
-const maxMana=5;
+const maxMana = 5;
 
 interface IUserInfo {
   nickname: string;
   img: string;
   numberLife: number;
   numberMana: number;
-  hisTurn: boolean
+  hisTurn: boolean;
 }
 
 const testUser = {
-  nickname: "Ender",
+  nickname: 'Ender',
   img: 'https://www.blast.hk/attachments/64804/',
   numberLife: 2,
   numberMana: 1,
-  hisTurn: true
-}
+  hisTurn: true,
+};
 const testUser2 = {
-  nickname: "Protoch",
+  nickname: 'Protoch',
   img: 'https://www.blast.hk/attachments/64804/',
   numberLife: 3,
   numberMana: 2,
-  hisTurn: true
-}
+  hisTurn: true,
+};
 
 function UserInfoDisplay() {
   return (
-    <div className='usersInfoBar'>
+    <div className="usersInfoBar">
       <UserInfo userInfo={testUser}/>
       <UserInfo userInfo={testUser2}/>
     </div>
-  )
-    ;
+  );
 }
 
-function UserInfo({userInfo}:{ userInfo : IUserInfo}) {
-  const hearts = [...Array(maxHealth)].map((_, i) =>
-    <img src={userInfo.numberLife > i ? fullLife : emptyLife} key={i} alt='' height='23px' width='33px'/>)
-  const mana = [...Array(maxMana)].map((_, i) =>
-    <img src={userInfo.numberMana > i ? fullMana : emptyMana} alt='' height='30px' width='40px'/>)
+function UserInfo({userInfo}: { userInfo: IUserInfo }) {
+  const hearts = [...Array(maxHealth)].map((_, i) => (
+    <img
+      src={userInfo.numberLife > i ? fullLife : emptyLife}
+      key={i}
+      alt=""
+      height="23px"
+      width="33px"
+    />
+  ));
+  const mana = [...Array(maxMana)].map((_, i) => (
+    <img
+      src={userInfo.numberMana > i ? fullMana : emptyMana}
+      alt=""
+      height="30px"
+      width="40px"
+    />
+  ));
   return (
     <div className="userInfo">
-      <img src={userInfo.img} className="userAvatar" alt=''/>
-      <div className='userData'>
-        <p className="userNickname">
-          {userInfo.nickname}
-        </p>
-        <div className='userIconBar'>
-          {hearts}
-        </div>
-        <div className='userIconBar'>
-          {mana}
-        </div>
+      <img src={userInfo.img} className="userAvatar" alt=""/>
+      <div className="userData">
+        <p className="userNickname">{userInfo.nickname}</p>
+        <div className="userIconBar">{hearts}</div>
+        <div className="userIconBar">{mana}</div>
       </div>
     </div>
   );
